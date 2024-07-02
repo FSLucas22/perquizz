a91b8ca818e8678757fe62af2e0ab9cfadcdb9b4
FSLucas22 <lucassantos@presconinformatica.com.br>
2024-07-02 03:06:46 -0300

    remove attribute `entityManager` from `MigrationController`

--- src/main/kotlin/com/perquizz/perquizz/migrations/controllers/MigrationController.kt
+++ src/main/kotlin/com/perquizz/perquizz/migrations/controllers/MigrationController.kt
@@ -2,13 +2,12 @@ package com.perquizz.perquizz.migrations.controllers
 
 import com.perquizz.perquizz.migrations.dtos.MigrationsResponseDto
 import com.perquizz.perquizz.migrations.services.MigrationService
-import jakarta.persistence.EntityManager
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class MigrationController(val service: MigrationService, val entityManager: EntityManager) {
+class MigrationController(val service: MigrationService) {
     @GetMapping("/api/v1/migrations")
     fun getMigrations(): ResponseEntity<MigrationsResponseDto> =
         ResponseEntity.ok(service.getPendingMigrations())
